e23a718f78364eabba07181d9f2eb65a22ea36d1
Drew Mercer <japanexpress@gmail.com>
2017-08-06 22:49:34 -0400

    updating main index and firebase setup

---
+++ assets/javascript/trainjs.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAaa2P63AZv-BdeaP2Kkzh2W_o3x38hvTM",
+    authDomain: "train-schedule-87f70.firebaseapp.com",
+    databaseURL: "https://train-schedule-87f70.firebaseio.com",
+    projectId: "train-schedule-87f70",
+    storageBucket: "train-schedule-87f70.appspot.com",
+    messagingSenderId: "1042683859824"
+  };
+  firebase.initializeApp(config);
+
